964fce6b512ef4dc780445367d3001352d12967b
yuezihao <739331673@qq.com>
2018-01-13 17:34:17 +0800

    update food view

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,13 @@
 <template>
-  <div v-show="showFlag" class="food">
-  </div>
+  <transition name="move">
+    <div v-show="showFlag" class="food">
+      <div class="food-content">
+				<div class="image-header">
+					<img :src="food.image" />
+				</div>
+			</div>
+    </div>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
@@ -27,7 +34,24 @@ export default {
 .food
 	position fixed
 	left 0
+	top 0
 	bottom 48px
 	z-index 30
+	width 100%
 	background #fff
+	&.move-enter-active, &.move-leave-active
+		transition all 0.2s linear
+	&.move-enter, &.move-leave-active
+		transform translate3d(100%, 0, 0)
+	.image-header
+		position relative
+		width 100%
+		height 0
+		padding-top 100%
+		img
+			position absolute
+			top 0
+			left 0
+			width 100%
+			height 100%
 </style>
