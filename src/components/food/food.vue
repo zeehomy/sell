3bdfba5f1bf846a24b856a148bfe0b8645d25ed8
yuezihao <739331673@qq.com>
2018-02-01 21:00:54 +0800

    modify food css

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -92,83 +92,59 @@ export default {
 	z-index: 30;
 	width: 100%;
 	background: #fff;
-
 	&.move-enter-active, &.move-leave-active
 		transition: all 0.2s linear;
-
 	&.move-enter, &.move-leave-active
 		transform: translate3d(100%, 0, 0);
-
 	.image-header
 		position: relative;
 		width: 100%;
 		height: 0;
 		padding-top: 100%;
-
 		img
 			position: absolute;
 			top: 0;
 			left: 0;
 			width: 100%;
 			height: 100%;
-
-
 		.back
 			position: absolute;
 			top: 10px;
 			left: 0;
-
 			.icon-arrow_lift
 				display: block;
 				padding: 10px;
 				font-size: 20px;
 				color: #fff;
-	
-
-
 	.content
 		padding: 18px;
-
 		.title
 			line-height: 14px;
 			margin-bottom: 8px;
 			font-size: 14px;
 			font-weight: 700;
 			color: rgb(7, 17, 27);
-
-
 		.detail
 			margin-bottom: 18px;
 			line-height: 10px;
 			height: 10px;
 			font-size: 0;
-
 			.sell-count, .rating
 				font-size: 10px;
 				color: rgb(147, 153, 159);
-	
-
 			.sell-count
 				margin-right: 12px;
-	
-
-
 		.price
 			font-weight: 700;
 			line-height: 24px;
-
 			.now
 				margin-right: 8px;
 				font-size: 14px;
 				color: rgb(240, 20, 20);
-	
-
 			.old
 				text-decoration: line-through;
 				font-size: 10px;
 				color: rgb(147, 153, 159);
-	
-
 	.cartcontrol-wrapper
 		position absolute
 		right 12px
