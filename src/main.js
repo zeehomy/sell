4e0196f42b0e1142895fa962d53fcdd1e7864cc2
yuezihao <739331673@qq.com>
2018-02-08 11:46:50 +0800

    add fundebug-javascript

--- src/main.js
+++ src/main.js
@@ -7,7 +7,32 @@ import Resource from 'vue-resource';
 
 import '@/common/stylus/index.styl';
 
+var fundebug = require('fundebug-javascript');
+fundebug.apikey = 'API-KEY';
+
 Vue.config.productionTip = false;
+
+function formatComponentName(vm) {
+  if (vm.$root === vm) return 'root';
+
+  var name = vm._isVue ? (vm.$options && vm.$options.name) || (vm.$options && vm.$options._componentTag) : vm.name;
+  return (name ? 'component <' + name + '>' : 'anonymous component') + (vm._isVue && vm.$options && vm.$options.__file ? ' at ' + (vm.$options && vm.$options.__file) : '');
+}
+
+Vue.config.errorHandler = function (err, vm, info) {
+  var componentName = formatComponentName(vm);
+  var propsData = vm.$options && vm.$options.propsData;
+
+  fundebug.notifyError(err,
+    {
+      metaData:
+        {
+          componentName: componentName,
+          propsData: propsData,
+          info: info
+        }
+    });
+};
 
 Vue.use(Resource);
 
